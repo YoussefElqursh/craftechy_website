7f8085b74e796030a36e6349754f35fa99a299d9
Youssef Ramadan <79144849+YoussefElqursh@users.noreply.github.com>
2025-11-02 12:00:23 +0200

    handel error

--- lib/feature/home/ui/widget/why_us_part/why_us_widget.dart
+++ lib/feature/home/ui/widget/why_us_part/why_us_widget.dart
@@ -97,85 +97,82 @@ class WhyUsWidget extends StatelessWidget {
               ],
             ),
           )
-        : Expanded(
-            flex: 1,
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 1,
-                  strokeAlign: BorderSide.strokeAlignOutside,
-                  color: AppColor.grayColor15,
-                ),
-              ),
-              padding: const EdgeInsets.all(60),
-              child: Column(
+        : Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              strokeAlign: BorderSide.strokeAlignOutside,
+              color: AppColor.grayColor15,
+            ),
+          ),
+          padding: const EdgeInsets.all(60),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 30,
+            children: [
+              Row(
                 mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 30,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 14,
                 children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    spacing: 14,
-                    children: [
-                      Container(
-                        width: 70.w,
-                        height: 70.h,
-                        padding: EdgeInsets.all(20.w),
-                        clipBehavior: Clip.antiAlias,
-                        decoration: ShapeDecoration(
-                          image: DecorationImage(image: svg.Svg(icon)),
-                          gradient: LinearGradient(
-                            begin: Alignment(0.00, 1.00),
-                            end: Alignment(0.00, 0.00),
-                            colors: [
-                              const Color(0xFF242424),
-                              const Color(0x00242424),
-                            ],
-                          ),
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                              width: 1,
-                              color: const Color(0xFF2E2E2E),
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
+                  Container(
+                    width: 70.w,
+                    height: 70.h,
+                    padding: EdgeInsets.all(20.w),
+                    clipBehavior: Clip.antiAlias,
+                    decoration: ShapeDecoration(
+                      image: DecorationImage(image: svg.Svg(icon)),
+                      gradient: LinearGradient(
+                        begin: Alignment(0.00, 1.00),
+                        end: Alignment(0.00, 0.00),
+                        colors: [
+                          const Color(0xFF242424),
+                          const Color(0x00242424),
+                        ],
                       ),
-                      SizedBox(
-                        width: 170.w,
-                        child: Text(
-                          title,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.sp,
-                            fontWeight: FontWeightHelper.medium,
-                            height: 1.50,
-                            letterSpacing: -0.12,
-                          ),
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          width: 1,
+                          color: const Color(0xFF2E2E2E),
                         ),
+                        borderRadius: BorderRadius.circular(8),
                       ),
-                    ],
+                    ),
                   ),
                   SizedBox(
-                    width: 400.w,
+                    width: 170.w,
                     child: Text(
-                      description,
+                      title,
                       style: TextStyle(
-                        color: AppColor.grayColor90,
-                        fontSize: 16.sp,
-                        fontWeight: FontWeightHelper.regular,
+                        color: Colors.white,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeightHelper.medium,
                         height: 1.50,
-                        letterSpacing: -0.10,
+                        letterSpacing: -0.12,
                       ),
                     ),
                   ),
                 ],
               ),
-            ),
-          );
+              SizedBox(
+                width: 400.w,
+                child: Text(
+                  description,
+                  style: TextStyle(
+                    color: AppColor.grayColor90,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeightHelper.regular,
+                    height: 1.50,
+                    letterSpacing: -0.10,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
   }
 }
